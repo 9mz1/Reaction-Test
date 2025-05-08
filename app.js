0fbb96f7408a1e1235dcc5aca283972ca469a86b
9mz1 <muzyyyyyyy@gmail.com>
2025-05-08 18:32:15 +0400

    Added A restart Function

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-info = document.querySelector('#info');
+testInfo = document.querySelector('#info');
+box = document.querySelector('.container');
 
-function time() {
-    info.textContent = 'clicked';
-    setTimeout(() => {
-        info.textContent = 'click NOW';
-        let elapsedTime = performance.now()
-        document.addEventListener('click', () => {
-            const reactionTime = (performance.now() - elapsedTime).toFixed(0);
-            info.textContent = `${reactionTime}ms`;
-        })
-    }, 5000);
+let isGreen = false;
+
+let startTime = Math.floor(Math.random() * 5000);
+
+function startTest() {
+    if (!isGreen) {
+        testInfo.textContent = 'wait for green';
+        setTimeout(() => {
+            testInfo.textContent = 'click now';
+            box.style.backgroundColor = 'green';
+            isGreen = true;
+            let elapsedTime = performance.now()
+            document.addEventListener('click', () => {
+                const reactionTime = (performance.now() - elapsedTime).toFixed(0);
+                testInfo.textContent = `${reactionTime}ms`;
+                restartTest()
+            }, { once: true });
+        }, startTime);
+    } else {
+        testInfo.textContent = 'too early';
+        box.style.backgroundColor = 'red';
+    }
+
 }
 
-(() => {
-    document.addEventListener('click', time);
-})();
+function restartTest() {
+    document.addEventListener('click', () => {
+        box.style.backgroundColor= 'red';
+        isGreen = false
+        info.textContent = "Click Here";
+        document.addEventListener('click', startTest, { once: true } );
+    });   
+}
+
+document.addEventListener('click', startTest, { once: true } );
